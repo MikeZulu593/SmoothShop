b952f2d3f3eb738d09f1c7cc0d2ce2e712c36da4
Mike Zulu <flightwatcher17@gmail.com>
2024-02-03 16:43:30 -0500

    Primer commit

--- App.js
+++ App.js
@@ -1,20 +1,108 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { TextInput, ImageBackground, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const image = require('./assets/background.png');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={image} style={styles.image}>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Bienvenido</Text>
+        <Text style={styles.subtitulo}>Cree su cuenta para comenzar</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Nombre"
+          placeholderTextColor="white"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Apellido"
+          placeholderTextColor="white"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Correo Electrónico"
+          placeholderTextColor="white"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          placeholderTextColor="white"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Confirmar Constrseña"
+          placeholderTextColor="white"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Número Telefónico"
+          placeholderTextColor="white"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Fecha de Nacimiento"
+          placeholderTextColor="white"
+        />
+        <Text style={styles.subtitulo}>Género</Text>
+        <Picker style={styles.input}>
+          <Picker.Item label="Masculino" value="1" color="white"/>
+          <Picker.Item label="Femenino" value="2" color="white"/>
+          <Picker.Item label="Otro" value="3" color="white"/>
+        </Picker>
+
+        <Text style={styles.subtitulo}>Términos y condiciones</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Aceptar</Text>
+        </TouchableOpacity>
+
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titulo: {
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 20,
+  },
+  subtitulo: {
+    fontWeight: 'normal',
+    color: 'white',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 2,
+    borderColor: '#bbbbbb',
+    borderRadius: 6,
+    backgroundColor: '#666666',
+    marginVertical: 5,
+    paddingHorizontal: 10,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    width: '100%',  
+    height: '100%', 
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#FFFFFF',
   },
 });
